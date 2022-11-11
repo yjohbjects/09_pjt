780a499fb81e48f8a095bc74d95b6dbf2daab475
xguu <sjjj.lim@gmail.com>
2022-11-11 09:43:49 +0900

    add_components

---
+++ my-vue-pjt/src/components/MovieCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieCard',
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-vue-pjt/src/components/WatchListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WatchListItem',
+}
+</script>
+
+<style>
+
+</style>
